1ca3fb4aa12e6b26589e039d9d2fa2bdeb6654f8
Diana Michelle <geekmlle@gmail.com>
2015-01-24 22:30:33 -0500

    Empty project
    Working on the record label app from the Designing iOS apps in
    Objective-C

---
+++ Ch3 SpinCity/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Ch3 SpinCity
+//
+//  Created by Diana Michelle on 1/24/15.
+//  Copyright (c) 2015 DianaMichelle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Ch3 SpinCity/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  Ch3 SpinCity
+//
+//  Created by Diana Michelle on 1/24/15.
+//  Copyright (c) 2015 DianaMichelle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
